845b0a7914eb042b2afc3f589dcf9d9c264c79f0
Sean Rose <sean1rose@gmail.com>
2014-05-14 14:59:12 -0700

    Start server

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by rexrose on 5/14/14.
+ */
+
+// import the server.js module, save it to local variable, start the server by calling the function in that module
+var server = require("./server");
+server.start();
